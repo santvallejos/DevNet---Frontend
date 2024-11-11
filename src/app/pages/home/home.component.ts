b0dd332703fb090c2b85482e6c8d1a8f8ad316f8
santvallejos <vallejossantiago1412@gmail.com>
2024-11-11 10:55:36 -0300

    Correcion en el home y componente publication

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,13 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { FlowbiteService } from '../../services/flowbite.service';
 /* Components ux/ui */
 import { SidebarComponent } from '../../core/sidebar/sidebar.component';
-import { ButtonLikeComponent } from "../../shared/button-like/button-like.component";
-import { ButtonSaveComponent } from "../../shared/button-save/button-save.component";
+import { PublicationComponent } from '../../core/publication/publication.component';
+
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [SidebarComponent, ButtonLikeComponent, ButtonSaveComponent],
+  imports: [SidebarComponent, PublicationComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
