c1bd9909dadc7c46dc73a4949dd470c2fff48445
santvallejos <vallejossantiago1412@gmail.com>
2024-11-11 00:02:29 -0300

    Diseño del home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,11 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { FlowbiteService } from '../../services/flowbite.service';
 /* Components ux/ui */
 import { SidebarComponent } from '../../core/sidebar/sidebar.component';
+import { ButtonLikeComponent } from "../../shared/button-like/button-like.component";
+import { ButtonSaveComponent } from "../../shared/button-save/button-save.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [SidebarComponent],
+  imports: [SidebarComponent, ButtonLikeComponent, ButtonSaveComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
