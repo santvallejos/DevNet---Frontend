4f36e447e5834aa7b4e4d91204f99f76df3d44a2
Santiago Vallejos <vallejossantiago1412@gmail.com>
2024-11-29 17:27:31 -0300

    Modificacion imgs

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,5 +3,5 @@ export const environment = {
     
     //'https://7666-181-88-211-147.ngrok-free.app/api'
     //'https://localhost:7224/api'
-    base_url:'https://7666-181-88-211-147.ngrok-free.app/api'
+    base_url:'https://localhost:7224/api'
 }
