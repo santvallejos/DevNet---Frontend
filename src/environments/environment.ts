c524bafcaba737956b7bebb675debf83cc5dbfb9
EMRocioB <rocioburguener@gmail.com>
2024-11-29 16:58:21 -0300

    Actualizacion de fondo de de imagen

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,6 @@ export const environment = {
     productio: true,
     
     //'https://7666-181-88-211-147.ngrok-free.app/api'
-    //'https://localhost:7224/api'
-    base_url:'https://7666-181-88-211-147.ngrok-free.app/api'
+    base_url:  'https://localhost:7224/api'
+  //  base_url:'https://7666-181-88-211-147.ngrok-free.app/api'
 }
