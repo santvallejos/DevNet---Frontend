aea2d9df85c695e4162a435eb4282b2e2c93cb4e
Santiago Vallejos <vallejossantiago1412@gmail.com>
2024-11-29 17:39:31 -0300

    Prueba deploy

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,5 +3,5 @@ export const environment = {
     
     //'https://7666-181-88-211-147.ngrok-free.app/api'
     //'https://localhost:7224/api'
-    base_url:'https://localhost:7224/api'
+    base_url:'https://2456-190-183-130-118.ngrok-free.app/api'
 }
