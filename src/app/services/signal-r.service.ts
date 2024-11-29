c3f1e600eeca966cf499dab7f2c06d1da376d9dc
JuanMumbach <juanimumbach@gmail.com>
2024-11-29 03:39:50 -0300

    messages.component.html arreglado, los mensajes se muestran correctamente

--- src/app/services/signal-r.service.ts
+++ src/app/services/signal-r.service.ts
@@ -8,7 +8,7 @@ export class SignalRService {
   //correo del usuario
   senderEmail: string = '';
   hubConnection: signalR.HubConnection;
-  private hubUrl = 'https://7666-181-88-211-147.ngrok-free.app/MessageHub'; //https://locahost:7224/MessageHub';                    
+  private hubUrl = 'https://7666-181-88-211-147.ngrok-free.app/MessageHub'; //https://localhost:7224/MessageHub';                    
   //Lista de usuarios conectados
   connectedUsers: string[] = [];
   receiverEmail: string = "";
